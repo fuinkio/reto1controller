85d30c95dc9e39b095166ba32949d74a5afefc0b
juan fernando bustamante zapata <jbustam9@eafit.edu.co>
2016-02-28 13:35:23 -0500

    op added to dataaccess

--- dataAccess.php
+++ dataAccess.php
@@ -3,18 +3,30 @@ include 'connection.php';
 header("Access-Control-Allow-Origin: *");
 
 
-
-$sql='SELECT * FROM tbl_user';
+ switch ($_GET['op']) {   
+    case 1:
+    //traer el carro por la placa
+       $sql="SELECT * FROM tbl_user WHERE tbl_user_username='".$_GET['user']."'";
  
-$result=$conn->query($sql);
+		$result=$conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // oupsut data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["tbl_user_id"]. "<br>";
+			if ($result->num_rows > 0) {
+    			// oupsut data of each row
+    			while($row = $result->fetch_assoc()) {
+        			if($row["tbl_user_password"]==$_GET['user']){
+        				echo json_encode($row);
+        			}else{
+        				echo "passerror";
+        			}
+    			}
+			} else {
+    		echo "usererror";
+			}
+         break;
+    default:
+    //Mensaje default si no se ingresa OP
+       echo "se necesita identificar la operacion";
+        break;
     }
-} else {
-    echo "0 results";
-}
 
 ?>
