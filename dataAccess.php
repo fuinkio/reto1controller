fe7b24d35987f6d9fefd10cd422d2c892403843a
juan fernando bustamante zapata <jbustam9@eafit.edu.co>
2016-02-28 14:17:45 -0500

    new board

--- dataAccess.php
+++ dataAccess.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 
  switch ($_GET['op']) {   
     case 1:
-    //traer el carro por la placa
+    //login
        $sql="SELECT * FROM tbl_user WHERE tbl_user_username='".$_GET['user']."'";
  
 		$result=$conn->query($sql);
@@ -25,8 +25,20 @@ header("Access-Control-Allow-Origin: *");
 			}
          break;
     case 2:
-    //traer el carro por la placa
+    //nuevo usuario
        $sql="INSERT INTO tbl_user VALUES (NULL, '".$_GET['user']."', '".$_GET['pass']."')";
+ 
+		$result=$conn->query($sql);
+			if ($result!=1) {
+    			// oupsuss ssata of each row
+    				echo "error";
+			} else {
+    		        	echo "success";
+			}
+         break;
+     case 3:
+    //nuevo tablero
+       $sql="INSERT INTO tbl_board  VALUES (NULL, '".$_GET['id']."', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";
  
 		$result=$conn->query($sql);
 			if ($result!=1) {
@@ -41,5 +53,6 @@ header("Access-Control-Allow-Origin: *");
        echo "se necesita identificar la operacion";
         break;
     }
+
 
 ?>
